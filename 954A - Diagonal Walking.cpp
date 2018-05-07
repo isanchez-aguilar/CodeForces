a7b94106dc6aaf55bc7ffe1f7b9d2ec5162c250c
isanchez-aguilar <saais31@gmail.com>
2018-05-07 01:02:53 -0500

    954A - Diagonal Walking
    954A - Diagonal Walking

---
+++ 954A - Diagonal Walking.cpp
@@ -0,0 +1,35 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: CodeForces 954A - Diagonal Walking
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	string s;
+	cin >> n >> s;
+	
+	bool used = false;
+	int cnt = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (used)
+		{
+			used = false;
+			continue;
+		}
+		else if ((s[i] == 'R' and s[i + 1] == 'U') or (s[i] == 'U' and s[i + 1] == 'R'))
+			++i;
+		
+		++cnt;
+	}
+	
+	cout << cnt << "\n";
+	return 0;
+}
