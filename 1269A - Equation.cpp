c292535be1c3cfeafd1b31a4c92154ed0ee4698a
Isaac Sanchez Aguilar <saais31@gmail.com>
2019-12-31 02:25:09 -0600

    1269A - Equation

---
+++ 1269A - Equation.cpp
@@ -0,0 +1,22 @@
+/*
+*	User: Isanchez_Aguilar
+*	Problem: CodeForces 1269A - Equation
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+using Long =  long long;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	Long n;
+	cin >> n;
+
+	cout << 9ll * n << " " << 8ll * n << "\n";
+
+	return 0;
+}
